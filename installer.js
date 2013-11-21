af8de97c80c55fa9b16a119eb5ae1fcca1bdbcf3
Kenneth Kouot <kenneth@wikia-inc.com>
2013-11-21 03:55:34 +0000

    create install script for global npm modules, add editorconfig and jshint, update gitignore

---
+++ installer.js
@@ -0,0 +1,26 @@
+var sudo = require( 'sudo' );
+var options = {
+  cachePassword: true,
+  prompt: 'Enter your sudo password to continue installation of global npm modules!'
+};
+
+var npmInstall = [
+  'npm', 'install', '-g'
+];
+
+var dependencies = [
+  'bower', 'jshint', 'sails'
+];
+
+var children = [];
+
+dependencies.forEach(function( dep ) {
+  var child = sudo( npmInstall.concat([ dep ]), options );
+  child.stdout.on( 'data', function( data ) {
+    console.log( data.toString() );
+  });
+  children.push( child );
+});
+
+
+
