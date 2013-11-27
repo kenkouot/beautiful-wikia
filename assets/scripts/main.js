eaf8a4e3bea78e2f7bd6ab05a7e5f76f1b70907e
Kenneth Kouot <kenneth@wikia-inc.com>
2013-11-27 01:15:22 -0800

    wrap require setup logic in closure

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,50 +2,52 @@
  * main.js
  * @description Sets up app dependencies and exports various globals
  */
-var path,
-    componentsPath,
-    env;
+(function( window ) {
+  var path,
+      componentsPath,
+      env;
 
-// Get environment
-env                  = window.wikia.env;
-// Store paths
-path                 = {};
-componentsPath       = '../components/';
-// Path to Bower Components
-path.angular         = componentsPath + 'angular/angular';
-path.angularResource = componentsPath + 'angular-resource/angular-resource';
-path.jQuery          = componentsPath + 'jquery/jquery';
+  // Get environment
+  env                  = window.wikia.env;
+  // Store paths
+  path                 = {};
+  componentsPath       = '../components/';
+  // Path to Bower Components
+  path.angular         = componentsPath + 'angular/angular';
+  path.angularResource = componentsPath + 'angular-resource/angular-resource';
+  path.jQuery          = componentsPath + 'jquery/jquery';
 
-if ( env === 'production' ) {
-  // ship preminified versions in prod environment
-  path.forEach(function( path ) {
-    path += '.min';
+  if ( env === 'production' ) {
+    // ship preminified versions in prod environment
+    path.forEach(function( path ) {
+      path += '.min';
+    });
+  }
+
+  require.config({
+    paths: {
+      angular: path.angular,
+      angularResource: path.angularResource,
+      jQuery: path.jQuery
+    },
+    shim: {
+      angular: {
+        exports: 'angular',
+        deps: [ 'jQuery' ]
+      },
+      angularResource: {
+        deps: [ 'angular' ]
+      },
+      jQuery: {
+        exports: '$'
+      }
+    },
+    priority: [
+      'angular'
+    ],
+    deps: [
+      // manually initialize angular app on Require is ready
+      'bootstrap'
+    ]
   });
-}
-
-require.config({
-  paths: {
-    angular: path.angular,
-    angularResource: path.angularResource,
-    jQuery: path.jQuery
-  },
-  shim: {
-    angular: {
-      exports: 'angular',
-      deps: [ 'jQuery' ]
-    },
-    angularResource: {
-      deps: [ 'angular' ]
-    },
-    jQuery: {
-      exports: '$'
-    }
-  },
-  priority: [
-    'angular'
-  ],
-  deps: [
-    // manually initialize angular app on Require is ready
-    'bootstrap'
-  ]
-});
+}( this ));
