877807bf2a76c2fbfceb052479938e8251f55270
Kyle Florence <kyle.florence@gmail.com>
2013-12-13 15:45:01 -0800

    sticky icky

--- assets/scripts/directives/wkStickyIcky.js
+++ assets/scripts/directives/wkStickyIcky.js
@@ -6,19 +6,19 @@ define([
     return {
       restrict: 'A',
       link: function( $scope, $elem ) {
-        var $elemOffset = $elem.offset().top;
+        var topBarHeight = $('.top-bar').height();
+        var elemPaddingTop = parseInt($elem.css( 'padding-top' ).replace( /[^\d]+/g, '' ));
+        var elemOffsetTop = $elem.offset().top + elemPaddingTop + 4; // because
         $( window ).on( 'scroll', function() {
           var scrollHeight = $( this ).scrollTop();
-
-          if ( scrollHeight > $elemOffset ) {
+          if ( scrollHeight >= elemOffsetTop ) {
             var articleBottom = $('article').offset().top + $('article').height();
-
-            if (scrollHeight + $elem.height() >= articleBottom ) {
+            if ( scrollHeight + $elem.height() >= articleBottom ) {
               $elem.css('top', articleBottom - $elem.height());
               $elem.css('position', 'absolute');
               $elem.removeClass('sticky');
             } else {
-              $elem.css('top', '');
+              $elem.css('top', topBarHeight);
               $elem.css('position', '');
               $elem.addClass('sticky');
             }
