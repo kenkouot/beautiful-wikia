95d9686e383c9d2e90d908b68b96386386af2125
Earl Carlson <theearlcarlson@gmail.com>
2013-12-15 12:49:27 -0800

    adding search overlay

---
+++ assets/scripts/directives/wkSearchOverlay.js
@@ -0,0 +1,24 @@
+/**
+ * wkHeroImage.js
+ * @description Directive for article hero image
+ */
+define([
+  'directives/module'
+], function( exports ) {
+  'use strict';
+  exports.directive( 'wkSearchOverlay', function() {
+    return {
+      /**
+       * @param {Object} scope refers to the BodyController, or whatever controller is in scope
+       * @param {jQuery Object} $elem is a jQuery object of the element you are returning, for instance, <wk-hero-image>
+       * @param {Object} attrs are the attributes on the custom element
+       */
+      restrict: 'E',
+      templateUrl: 'search-overlay.html',
+      link: function( scope, $elem, attrs ) {
+        // * do stuff here *
+        console.log( $elem );
+      }
+    };
+  });
+});
